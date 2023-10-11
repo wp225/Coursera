f8f60aab6616f1aa795b4653bd64d28a6d84a469
jeorgejoshi <85611495+wp225@users.noreply.github.com>
2023-10-11 17:23:21 +0545

    updated utils for load/show image and padding

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print('test')
+from utils.img import image
+from utils.padding import zero_padding
+img_obj=image('./test_image.webp')
+img=img_obj.load_image()
+zero_padding(img)
---
+++ utils/padding.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+
+def zero_padding(image):
+    h,w=image.shape[0],image.shape[1]
+    print(h,w)
