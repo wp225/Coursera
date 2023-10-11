9a48ff75a3a347661188d43ff8a0b100f284ebf1
jeorgejoshi <85611495+wp225@users.noreply.github.com>
2023-10-11 18:26:15 +0545

    updated padding

--- utils/padding.py
+++ utils/padding.py
@@ -1,6 +1,21 @@
 import cv2
 import numpy as np
 
-def zero_padding(image):
-    h,w=image.shape[0],image.shape[1]
-    print(h,w)
+def zero_padding(image,filter_size):
+    '''
+    :param image:
+    :return: padded image
+    '''
+    array=np.array(image)
+    h,w=array.shape[0],array.shape[1]
+    padding_req=int((filter_size-1)/2)
+    padded_image=np.array([h+padding_req,w+padding_req,3],dtype=np.uint8)
+    padded_h,padded_w=padded_image[0],padded_image[1]
+    padded_image[:,:]=np.array([0,64,128])
+    padded_image[padded_h:h,padded_w:w]=array
+    return padded_image
+
+if __name__ == '__main__':
+    image=cv2.imread('')
+    cv2.imshow('img',image)
+    #zero_padding(image,3)
