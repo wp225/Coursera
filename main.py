bfc0b80e0aee84455dd9f191675137206264abc7
jeorgejoshi <85611495+wp225@users.noreply.github.com>
2023-10-11 21:40:03 +0545

    updated padding

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import cv2
+import matplotlib.pyplot as plt
+
+# Load the padded image
+
+# Display the image
+
+
 from utils.img import image
 from utils.padding import zero_padding
 img_obj=image('./test_image.webp')
 img=img_obj.load_image()
-zero_padding(img)
+padded_image=zero_padding(img,3)
--- utils/padding.py
+++ utils/padding.py
@@ -9,13 +9,7 @@ def zero_padding(image,filter_size):
     array=np.array(image)
     h,w=array.shape[0],array.shape[1]
     padding_req=int((filter_size-1)/2)
-    padded_image=np.array([h+padding_req,w+padding_req,3],dtype=np.uint8)
-    padded_h,padded_w=padded_image[0],padded_image[1]
-    padded_image[:,:]=np.array([0,64,128])
-    padded_image[padded_h:h,padded_w:w]=array
+    padded_h,padded_w=h+2*padding_req,w+2*padding_req
+    padded_image=np.zeros((padded_h,padded_w,3),dtype=array.dtype)
+    padded_image[padding_req:padding_req + h, padding_req:padding_req + w] = array
     return padded_image
-
-if __name__ == '__main__':
-    image=cv2.imread('./test_image.webp')
-    cv2.imshow('img',image)
-    #zero_padding(image,3)
